b8e9e497af672bfad6248b16e71babf589ee5dd1
Mithradatha <sam.kellar@outlook.com>
2017-03-05 17:05:38 -0500

    fixed UDPClient

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -1,46 +1,37 @@
+import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
-/**
- * Created by Nemahs on 3/5/2017.
- */
 public class UDPClient {
+
     private static int PACKET_SIZE = 1024;
 
-    public static void main(String[] args)
-    {
-        try {
-            int port = Integer.parseInt(args[1]);
-            InetAddress addr = InetAddress.getByName(args[0]);
-            DatagramSocket sock = new DatagramSocket(port, addr);
-            Scanner cin = new Scanner(System.in);
+    public static void main(String[] args) {
 
+        try (
+                DatagramSocket clientSocket = new DatagramSocket();
+                Scanner sc = new Scanner(System.in);
+        ) {
+            //int port = Integer.parseInt(args[1]);
+            //InetAddress addr = InetAddress.getByName(args[0]);
 
-            while (cin.hasNext())
-            {
-                String command = cin.nextLine();
-                DatagramPacket pack = new DatagramPacket(command.getBytes(), command.length(), addr, port);
-                sock.send(pack);
+            int port = 2345;
+            InetAddress addr = InetAddress.getByName("localhost");
 
-                DatagramPacket message = new DatagramPacket(new byte[PACKET_SIZE], PACKET_SIZE);
-                sock.receive(message);
-                System.out.println(new String(pack.getData()).split("\n")[0]);
+            String stdIn;
+            while ((stdIn = sc.nextLine()) != null) {
+                DatagramPacket sendPacket = new DatagramPacket(stdIn.getBytes(), stdIn.length(), addr, port);
+                clientSocket.send(sendPacket);
+                DatagramPacket receivePacket = new DatagramPacket(new byte[PACKET_SIZE], PACKET_SIZE);
+                clientSocket.receive(receivePacket);
+                String message = new String(receivePacket.getData(), "UTF-8").split("\n")[0];
+                System.out.println(message);
             }
 
-
-        }
-        catch (java.net.SocketException e)
-        {
-            System.err.println(e.getMessage());
-        }
-        catch (java.net.UnknownHostException e)
-        {
-            System.err.println(e.getMessage());
-        }
-        catch (java.io.IOException e)
-        {
+        } catch (java.io.IOException e) {
             System.err.println(e.getMessage());
         }
     }
