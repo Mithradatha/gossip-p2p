922ad28c77784f6fd8d7342e2443b4d3e04f9793
Luis Maura <robbymaura@gmail.com>
2017-03-21 11:27:02 -0400

    Multithreaded the TCPServer

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -1,20 +1,14 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.SQLException;
 
 class TCPServer implements Runnable {
 
     private int port;
-    private DataBaseHandler db;
     private Logger logger;
 
     TCPServer(int port) {
         this.port = port;
-        this.db = DataBaseHandler.getInstance();
         this.logger = Logger.getInstance();
     }
 
@@ -22,29 +16,12 @@ class TCPServer implements Runnable {
     public void run() {
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
-            for (; ; ) {
-                try (
-                        Socket clientSocket = serverSocket.accept();
-                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                ) {
-                    // Parser.GREETINGS
-                    out.println(Parser.GREETINGS);
-                    out.flush();
-
-                    logger.log(Parser.TCP, Parser.SERVER, Parser.GREETINGS);
-
-                    String request;
-                    while ((request = in.readLine()) != null) {
-                        logger.log(Parser.TCP, Parser.CLIENT, request);
-
-                        String response = Parser.ParseAndExecuteCommand(request, db);
-                        out.println(response);
-                        out.flush();
-
-                        logger.log(Parser.TCP, Parser.SERVER, response);
-                    }
-                } catch (IOException | SQLException ex) {
+            while (true) {
+                try {
+                    Socket clientSocket = serverSocket.accept();
+                    TCPClientHandler newHandler = new TCPClientHandler(clientSocket);
+                    newHandler.run();
+                } catch (IOException ex) {
                     logger.log(ex);
                 }
             }
